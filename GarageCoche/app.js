f54d94b253be72bf9c3edead947ae8cbe32b59c5
Alfonso Mulsa <amulsa@gmail.com>
2024-04-06 20:50:20 +0200

    Modificación proyecto GarageCoche

--- GarageCoche/app.js
+++ GarageCoche/app.js
@@ -27,12 +27,12 @@ function calcularMedia(...numeros) {
 let coche1 = new Coche("7643HT", 60, 105.8, "Seat 600");
 let coche2 = new Coche("4856KLG", 125, 187.9, "Volvo 678");
 let coche3 = new Coche("2967FED", 190, 245.7, "Mercedes 500");
-document.write(coche1.imprime());
-document.write(`Velocidad crucero: ${coche1.velocidadCrucero()}<br/><br/>`);
-document.write(coche2.imprime());
-document.write(`Velocidad crucero: ${coche2.velocidadCrucero()}<br/><br/>`);
-document.write(coche3.imprime());
-document.write(`Velocidad crucero: ${coche3.velocidadCrucero()}<br/><br/>`);
-document.write(`Velocidad media: ${calcularMedia(coche1.velocidad, coche2.velocidad, coche3.velocidad)}<br/>`);
-document.write(`Potencia media: ${calcularMedia(coche1.potencia, coche2.potencia, coche3.potencia)}<br/>`);
+//document.write(coche1.imprime());
+//document.write(`Velocidad crucero: ${coche1.velocidadCrucero()}<br/><br/>`);
+//document.write(coche2.imprime());
+//document.write(`Velocidad crucero: ${coche2.velocidadCrucero()}<br/><br/>`);
+//document.write(coche3.imprime());
+//document.write(`Velocidad crucero: ${coche3.velocidadCrucero()}<br/><br/>`);
+//document.write(`Velocidad media: ${calcularMedia(coche1.velocidad, coche2.velocidad, coche3.velocidad)}<br/>`);
+//document.write(`Potencia media: ${calcularMedia(coche1.potencia, coche2.potencia, coche3.potencia)}<br/>`);
 //# sourceMappingURL=app.js.map
