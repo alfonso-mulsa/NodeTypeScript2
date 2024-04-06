58b0b431217e63236f0c55585c6a0f0fd1143fab
Alfonso Mulsa <amulsa@gmail.com>
2024-04-06 14:00:45 +0200

    Uso de interfaces en el proyecto Herencias

--- Herencias/app.js
+++ Herencias/app.js
@@ -1,131 +1,54 @@
 "use strict";
-class Animal {
-    constructor(yearNacimiento, mesNacimiento, diaNacimiento, nombre) {
-        this._yearNacimiento = 0;
-        this._mesNacimiento = 0;
-        this._diaNacimiento = 0;
-        this._nombre = "";
-        this.yearNacimiento = yearNacimiento;
-        this.mesNacimiento = mesNacimiento;
-        this.diaNacimiento = diaNacimiento;
-        this.nombre = nombre;
-    }
-    get yearNacimiento() {
-        return this._yearNacimiento;
-    }
-    set yearNacimiento(value) {
-        if (value < 2000) {
-            this._yearNacimiento = 2000;
-        }
-        else if (value > 2024) {
-            this._yearNacimiento = 2024;
-        }
-        else {
-            this._yearNacimiento = value;
-        }
-    }
-    get mesNacimiento() {
-        return this._mesNacimiento;
-    }
-    set mesNacimiento(value) {
-        if (value < 1) {
-            this._mesNacimiento = 1;
-        }
-        else if (value > 12) {
-            this._mesNacimiento = 12;
-        }
-        else {
-            this._mesNacimiento = value;
-        }
-    }
-    get diaNacimiento() {
-        return this._diaNacimiento;
-    }
-    set diaNacimiento(value) {
-        if (value < 1) {
-            this._diaNacimiento = 1;
-        }
-        else if (value > 31) {
-            this._diaNacimiento = 31;
-        }
-        else {
-            this._diaNacimiento = value;
-        }
-    }
-    get nombre() {
-        return this._nombre;
-    }
-    set nombre(value) {
-        switch (value.length) {
-            case 0:
-                this._nombre = "Pi";
-                break;
-            case 1:
-                this._nombre = value + value;
-                break;
-            default:
-                this._nombre = value;
-        }
+class Fecha {
+    constructor(Anyo, Mes, Dia) {
+        this.anyo = 0;
+        this.mes = 0;
+        this.dia = 0;
+        this.anyo = Anyo;
+        this.mes = Mes;
+        this.dia = Dia;
     }
     dameEdad() {
         let hoy = new Date();
-        let edad = hoy.getFullYear() - this._yearNacimiento;
-        let m = hoy.getMonth() - this._mesNacimiento;
-        if (m < 0 || (m === 0 && hoy.getDate() < this._diaNacimiento)) {
+        let edad = hoy.getFullYear() - this.anyo;
+        let m = hoy.getMonth() - this.mes;
+        if (m < 0 || (m === 0 && hoy.getDate() < this.dia)) {
             edad--;
         }
         return edad;
     }
+    dameFecha() {
+        return `${this.anyo}/${this.mes}/${this.dia}`;
+    }
+}
+class Animal {
+    constructor(anyoNacimiento, mesNacimiento, diaNacimiento, nombre) {
+        this.nombre = "";
+        this.fechaNacimiento = new Fecha(anyoNacimiento, mesNacimiento, diaNacimiento);
+        this.nombre = nombre;
+    }
     dameDatos() {
-        return `Nombre: ${this._nombre}<br/>Fecha de nacimiento: ${this._diaNacimiento}/${this._mesNacimiento}/${this._yearNacimiento}<br/>Edad: ${this.dameEdad()}<br/>`;
+        return `Nombre: ${this.nombre}<br/>Fecha de nacimiento: ${this.fechaNacimiento.dameFecha()}<br/>Edad: ${this.fechaNacimiento.dameEdad()}<br/>`;
     }
 }
 class Mamifero extends Animal {
-    constructor(yearNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion) {
-        super(yearNacimiento, mesNacimiento, diaNacimiento, nombre);
-        this._mesesGestacion = 0;
+    constructor(anyoNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion) {
+        super(anyoNacimiento, mesNacimiento, diaNacimiento, nombre);
+        this.mesesGestacion = 0;
         this.mesesGestacion = mesesGestacion;
     }
-    get mesesGestacion() {
-        return this._mesesGestacion;
-    }
-    set mesesGestacion(value) {
-        if (value < 1) {
-            this._mesesGestacion = 1;
-        }
-        else if (value > 18) {
-            this._mesesGestacion = 18;
-        }
-        else {
-            this._mesesGestacion = value;
-        }
-    }
     dameDatos() {
-        return `${super.dameDatos()}Meses de gestacion: ${this._mesesGestacion}<br/>`;
+        return `${super.dameDatos()}Meses de gestacion: ${this.mesesGestacion}<br/>`;
     }
 }
 class Primate extends Mamifero {
-    constructor(yearNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion, masaCerebral) {
-        super(yearNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion);
-        this._masaCerebral = 0;
+    constructor(anyoNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion, masaCerebral) {
+        super(anyoNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion);
+        this.masaCerebral = 0;
         this.masaCerebral = masaCerebral;
     }
-    get masaCerebral() {
-        return this._masaCerebral;
-    }
-    set masaCerebral(value) {
-        if (value < 100) {
-            this._masaCerebral = 100;
-        }
-        else if (value > 2000) {
-            this._masaCerebral = 2000;
-        }
-        else {
-            this._masaCerebral = value;
-        }
-    }
     proporcionMasaCerebral() {
-        return Number((this._masaCerebral / this.mesesGestacion).toFixed(2));
+        return Number((this.masaCerebral / this.mesesGestacion).toFixed(2));
     }
     dameDatos() {
         return `${super.dameDatos()}Proporcion masa cerebral: ${this.proporcionMasaCerebral()}<br/>`;
@@ -138,9 +61,70 @@ class Humano extends Primate {
         this.apellidos = apellidos;
     }
     dameDatos() {
-        return `${super.dameDatos()}Apellidos: ${this.apellidos}<br/>`;
+        return `Apellidos: ${this.apellidos}<br/>${super.dameDatos()}`;
     }
 }
-let humano1 = new Humano(2003, 12, 18, "Pepito", 9, 1800, "Grillo");
-document.writeln(humano1.dameDatos());
+class ValidarFecha00_24 {
+    isValid(fecha) {
+        return fecha.anyo >= 2000 && fecha.anyo <= 2024 && fecha.mes >= 1 && fecha.mes <= 12 && fecha.dia >= 1 && fecha.dia <= 31;
+    }
+}
+class ValidarFecha80_20 {
+    isValid(fecha) {
+        return fecha.anyo >= 1980 && fecha.anyo <= 2020 && fecha.mes >= 1 && fecha.mes <= 12 && fecha.dia >= 1 && fecha.dia <= 31;
+    }
+}
+class ValidarFecha90_23 {
+    isValid(fecha) {
+        return fecha.anyo >= 1990 && fecha.anyo <= 2023 && fecha.mes >= 1 && fecha.mes <= 12 && fecha.dia >= 1 && fecha.dia <= 31;
+    }
+}
+class ValidarAnimal {
+    isValid(animal) {
+        return new ValidarFecha00_24().isValid(animal.fechaNacimiento) && animal.nombre.length >= 2;
+    }
+}
+class ValidarMamifero1_18 {
+    isValid(mamifero) {
+        return new ValidarFecha00_24().isValid(mamifero.fechaNacimiento) && mamifero.nombre.length >= 2 && mamifero.mesesGestacion >= 1 && mamifero.mesesGestacion <= 18;
+    }
+}
+class ValidarMamifero1_29 {
+    isValid(mamifero) {
+        return new ValidarFecha00_24().isValid(mamifero.fechaNacimiento) && mamifero.nombre.length >= 2 && mamifero.mesesGestacion >= 1 && mamifero.mesesGestacion <= 29;
+    }
+}
+class ValidarPrimate {
+    isValid(primate) {
+        return new ValidarFecha00_24().isValid(primate.fechaNacimiento) && primate.nombre.length >= 2 && primate.mesesGestacion >= 5 && primate.mesesGestacion <= 12
+            && primate.masaCerebral >= 100 && primate.masaCerebral <= 2000;
+    }
+}
+class ValidarHumano {
+    isValid(humano) {
+        return new ValidarFecha00_24().isValid(humano.fechaNacimiento) && humano.nombre.length >= 2 && humano.mesesGestacion >= 6 && humano.mesesGestacion <= 9
+            && humano.masaCerebral >= 1200 && humano.masaCerebral <= 2000;
+    }
+}
+// Creaci�n de los validadores
+let _ValidarAnimal = new ValidarAnimal();
+let _ValidarMamifero = new ValidarMamifero1_18();
+let _ValidarPrimate = new ValidarPrimate();
+let _ValidarHumano = new ValidarHumano();
+let animal1 = new Animal(2012, 8, 23, "Krasty");
+if (_ValidarAnimal.isValid(animal1)) {
+    document.write(`ANIMAL<br/>${animal1.dameDatos()}`);
+}
+let mamifero1 = new Mamifero(2019, 5, 11, "D", 12);
+if (_ValidarMamifero.isValid(mamifero1)) {
+    document.write(`MAMIFERO<br/>${mamifero1.dameDatos()}`);
+}
+let primate1 = new Primate(2014, 9, 29, "Hommer", 8, 1000);
+if (_ValidarPrimate.isValid(primate1)) {
+    document.write(`PRIMATE<br/>${primate1.dameDatos()}`);
+}
+let humano1 = new Humano(2003, 12, 18, "Pepito", 8, 1800, "Grillo");
+if (_ValidarHumano.isValid(humano1)) {
+    document.write(`HUMANO<br/>${humano1.dameDatos()}`);
+}
 //# sourceMappingURL=app.js.map
--- Herencias/app.ts
+++ Herencias/app.ts
@@ -1,122 +1,54 @@
-class Animal {
-    private _yearNacimiento: number = 0;
-    private _mesNacimiento: number = 0;
-    private _diaNacimiento: number = 0;
-    private _nombre: string = "";
-    constructor(yearNacimiento: number, mesNacimiento: number, diaNacimiento: number, nombre: string) {
-        this.yearNacimiento = yearNacimiento;
-        this.mesNacimiento = mesNacimiento;
-        this.diaNacimiento = diaNacimiento;
-        this.nombre = nombre;
-    }
-    public get yearNacimiento(): number {
-        return this._yearNacimiento;
-    }
-    public set yearNacimiento(value: number) {
-        if (value < 2000) {
-            this._yearNacimiento = 2000;
-        } else if (value > 2024) {
-            this._yearNacimiento = 2024;
-        } else {
-            this._yearNacimiento = value;
-        }
-    }
-    public get mesNacimiento(): number {
-        return this._mesNacimiento;
-    }
-    public set mesNacimiento(value: number) {
-        if (value < 1) {
-            this._mesNacimiento = 1;
-        } else if (value > 12) {
-            this._mesNacimiento = 12;
-        } else {
-            this._mesNacimiento = value;
-        }
-    }
-    public get diaNacimiento(): number {
-        return this._diaNacimiento;
-    }
-    public set diaNacimiento(value: number) {
-        if (value < 1) {
-            this._diaNacimiento = 1;
-        } else if (value > 31) {
-            this._diaNacimiento = 31;
-        } else {
-            this._diaNacimiento = value;
-        }
-    }
-    public get nombre(): string {
-        return this._nombre;
-    }
-    public set nombre(value: string) {
-        switch (value.length) {
-            case 0:
-                this._nombre = "Pi";
-                break;
-            case 1:
-                this._nombre = value + value;
-                break
-            default:
-                this._nombre = value;
-        }
+class Fecha {
+    anyo: number = 0;
+    mes: number = 0;
+    dia: number = 0;
+    constructor(Anyo: number, Mes: number, Dia: number) {
+        this.anyo = Anyo;
+        this.mes = Mes;
+        this.dia = Dia;
     }
     dameEdad(): number {
         let hoy = new Date();
-        let edad = hoy.getFullYear() - this._yearNacimiento;
-        let m = hoy.getMonth() - this._mesNacimiento;
-
-        if (m < 0 || (m === 0 && hoy.getDate() < this._diaNacimiento)) {
+        let edad = hoy.getFullYear() - this.anyo;
+        let m = hoy.getMonth() - this.mes;
+        if (m < 0 || (m === 0 && hoy.getDate() < this.dia)) {
             edad--;
         }
-
         return edad;
     }
+    dameFecha(): string {
+        return `${this.anyo}/${this.mes}/${this.dia}`;
+    }
+}
+class Animal {
+    fechaNacimiento: Fecha;
+    nombre: string = "";
+    constructor(anyoNacimiento: number, mesNacimiento: number, diaNacimiento: number, nombre: string) {
+        this.fechaNacimiento = new Fecha(anyoNacimiento, mesNacimiento, diaNacimiento);
+        this.nombre = nombre;
+    }
     dameDatos(): string {
-        return `Nombre: ${this._nombre}<br/>Fecha de nacimiento: ${this._diaNacimiento}/${this._mesNacimiento}/${this._yearNacimiento}<br/>Edad: ${this.dameEdad()}<br/>`;
+        return `Nombre: ${this.nombre}<br/>Fecha de nacimiento: ${this.fechaNacimiento.dameFecha()}<br/>Edad: ${this.fechaNacimiento.dameEdad()}<br/>`;
     }
 }
 class Mamifero extends Animal {
-    private _mesesGestacion: number = 0;
-    constructor(yearNacimiento: number, mesNacimiento: number, diaNacimiento: number, nombre: string, mesesGestacion: number) {
-        super(yearNacimiento, mesNacimiento, diaNacimiento, nombre);
+    mesesGestacion: number = 0;
+    constructor(anyoNacimiento: number, mesNacimiento: number, diaNacimiento: number, nombre: string, mesesGestacion: number) {
+        super(anyoNacimiento, mesNacimiento, diaNacimiento, nombre);
         this.mesesGestacion = mesesGestacion;
     }
-    public get mesesGestacion(): number {
-        return this._mesesGestacion;
-    }
-    public set mesesGestacion(value: number) {
-        if (value < 1) {
-            this._mesesGestacion = 1;
-        } else if (value > 18) {
-            this._mesesGestacion = 18;
-        } else {
-            this._mesesGestacion = value;
-        }
-    }
     dameDatos(): string {
-        return `${super.dameDatos()}Meses de gestacion: ${this._mesesGestacion}<br/>`;
+        return `${super.dameDatos()}Meses de gestacion: ${this.mesesGestacion}<br/>`;
     }
 }
 class Primate extends Mamifero {
-    private _masaCerebral: number = 0;
-    constructor(yearNacimiento: number, mesNacimiento: number, diaNacimiento: number, nombre: string, mesesGestacion: number, masaCerebral: number) {
-        super(yearNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion);
+    masaCerebral: number = 0;
+    constructor(anyoNacimiento: number, mesNacimiento: number, diaNacimiento: number, nombre: string, mesesGestacion: number, masaCerebral: number) {
+        super(anyoNacimiento, mesNacimiento, diaNacimiento, nombre, mesesGestacion);
         this.masaCerebral = masaCerebral;
     }
-    public get masaCerebral(): number {
-        return this._masaCerebral;
-    }
-    public set masaCerebral(value: number) {
-        if (value < 100) {
-            this._masaCerebral = 100;
-        } else if (value > 2000) {
-            this._masaCerebral = 2000;
-        } else {
-            this._masaCerebral = value;
-        }
-    }
     proporcionMasaCerebral(): number {
-        return Number((this._masaCerebral / this.mesesGestacion).toFixed(2));
+        return Number((this.masaCerebral / this.mesesGestacion).toFixed(2));
     }
     dameDatos(): string {
         return `${super.dameDatos()}Proporcion masa cerebral: ${this.proporcionMasaCerebral()}<br/>`;
@@ -129,9 +61,92 @@ class Humano extends Primate {
         this.apellidos = apellidos;
     }
     dameDatos(): string {
-        return `${super.dameDatos()}Apellidos: ${this.apellidos}<br/>`;
+        return `Apellidos: ${this.apellidos}<br/>${super.dameDatos()}`;
     }
 }
 
-let humano1 = new Humano(2003, 12, 18, "Pepito", 9, 1800, "Grillo");
-document.writeln(humano1.dameDatos());
+// Definición de los validadores
+interface IValidadorFecha {
+    isValid(fecha: Fecha): boolean;
+}
+class ValidarFecha00_24 implements IValidadorFecha {
+    isValid(fecha: Fecha): boolean {
+        return fecha.anyo >= 2000 && fecha.anyo <= 2024 && fecha.mes >= 1 && fecha.mes <= 12 && fecha.dia >= 1 && fecha.dia <= 31;
+    }
+}
+class ValidarFecha80_20 implements IValidadorFecha {
+    isValid(fecha: Fecha): boolean {
+        return fecha.anyo >= 1980 && fecha.anyo <= 2020 && fecha.mes >= 1 && fecha.mes <= 12 && fecha.dia >= 1 && fecha.dia <= 31;
+    }
+}
+class ValidarFecha90_23 implements IValidadorFecha {
+    isValid(fecha: Fecha): boolean {
+        return fecha.anyo >= 1990 && fecha.anyo <= 2023 && fecha.mes >= 1 && fecha.mes <= 12 && fecha.dia >= 1 && fecha.dia <= 31;
+    }
+}
+interface IValidadorAnimal {
+    isValid(animal: Animal): boolean;
+}
+class ValidarAnimal implements IValidadorAnimal {
+    isValid(animal: Animal): boolean {
+        return new ValidarFecha00_24().isValid(animal.fechaNacimiento) && animal.nombre.length >= 2;
+    }
+}
+interface IValidadorMamifero {
+    isValid(mamifero: Mamifero): boolean;
+}
+class ValidarMamifero1_18 implements IValidadorMamifero {
+    isValid(mamifero: Mamifero): boolean {
+        return new ValidarFecha00_24().isValid(mamifero.fechaNacimiento) && mamifero.nombre.length >= 2 && mamifero.mesesGestacion >= 1 && mamifero.mesesGestacion <= 18;
+    }
+}
+class ValidarMamifero1_29 implements IValidadorMamifero {
+    isValid(mamifero: Mamifero): boolean {
+        return new ValidarFecha00_24().isValid(mamifero.fechaNacimiento) && mamifero.nombre.length >= 2 && mamifero.mesesGestacion >= 1 && mamifero.mesesGestacion <= 29;
+    }
+}
+interface IValidadorPrimate {
+    isValid(primate: Primate): boolean;
+}
+class ValidarPrimate implements IValidadorPrimate {
+    isValid(primate: Primate): boolean {
+        return new ValidarFecha00_24().isValid(primate.fechaNacimiento) && primate.nombre.length >= 2 && primate.mesesGestacion >= 5 && primate.mesesGestacion <= 12
+            && primate.masaCerebral >= 100 && primate.masaCerebral <= 2000;
+    }
+}
+interface IValidadorHumano {
+    isValid(humano: Humano): boolean;
+}
+class ValidarHumano implements IValidadorHumano {
+    isValid(humano: Humano): boolean {
+        return new ValidarFecha00_24().isValid(humano.fechaNacimiento) && humano.nombre.length >= 2 && humano.mesesGestacion >= 6 && humano.mesesGestacion <= 9
+            && humano.masaCerebral >= 1200 && humano.masaCerebral <= 2000;
+    }
+}
+
+// Creación de los validadores
+let _ValidarAnimal: IValidadorAnimal = new ValidarAnimal();
+let _ValidarMamifero: IValidadorMamifero = new ValidarMamifero1_18();
+let _ValidarPrimate: IValidadorPrimate = new ValidarPrimate();
+let _ValidarHumano: IValidadorHumano = new ValidarHumano();
+
+// Creación de objetos y su validación
+let animal1 = new Animal(2012, 8, 23, "Krasty");
+if (_ValidarAnimal.isValid(animal1)) {
+    document.write(`ANIMAL<br/>${animal1.dameDatos()}`);
+}
+
+let mamifero1 = new Mamifero(2019, 5, 11, "D", 12);
+if (_ValidarMamifero.isValid(mamifero1)) {
+    document.write(`MAMIFERO<br/>${mamifero1.dameDatos()}`);
+}
+
+let primate1 = new Primate(2014, 9, 29, "Hommer", 8, 1000);
+if (_ValidarPrimate.isValid(primate1)) {
+    document.write(`PRIMATE<br/>${primate1.dameDatos()}`);
+}
+
+let humano1 = new Humano(2003, 12, 18, "Pepito", 8, 1800, "Grillo");
+if (_ValidarHumano.isValid(humano1)) {
+    document.write(`HUMANO<br/>${humano1.dameDatos()}`);
+}
