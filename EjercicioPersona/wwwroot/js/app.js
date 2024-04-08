7f5d756f6b817e359a13c99d5a709444b1867b92
Alfonso Mulsa <amulsa@gmail.com>
2024-04-08 23:04:09 +0200

    Creación proyecto EjercicioPersona

---
+++ EjercicioPersona/wwwroot/js/app.js
@@ -0,0 +1,114 @@
+var Persona = /** @class */ (function () {
+    function Persona() {
+        this.primerNombre = "";
+        this.nombreIntermedio = "";
+        this.apellido1 = "";
+        this.apellido2 = "";
+        this.anyoNacimiento = 0;
+        this.identificativo = "";
+        this.activo = false;
+        //constructor(primerNombre: string, nombreIntermedio: string, apellido1: string, apellido2: string, anyoNacimiento: number, identificativo: string, activo: boolean) {
+        //    this.primerNombre = primerNombre;
+        //    this.nombreIntermedio = nombreIntermedio;
+        //    this.apellido1 = apellido1;
+        //    this.apellido2 = apellido2;
+        //    this.anyoNacimiento = anyoNacimiento;
+        //    this.identificativo = identificativo;
+        //    this.activo = activo;
+        //}
+    }
+    return Persona;
+}());
+var CrearPersona = /** @class */ (function () {
+    function CrearPersona() {
+    }
+    CrearPersona.prototype.crearPersona = function (primerNombre, nombreIntermedio, apellido1, apellido2, anyoNacimiento, identificativo, activo) {
+        var nuevaPersona = new Persona;
+        nuevaPersona.primerNombre = primerNombre;
+        nuevaPersona.nombreIntermedio = nombreIntermedio;
+        nuevaPersona.apellido1 = apellido1;
+        nuevaPersona.apellido2 = apellido2;
+        nuevaPersona.anyoNacimiento = anyoNacimiento;
+        nuevaPersona.identificativo = identificativo;
+        nuevaPersona.activo = activo;
+        return nuevaPersona;
+    };
+    return CrearPersona;
+}());
+var ValidarPersonaInglesa = /** @class */ (function () {
+    function ValidarPersonaInglesa() {
+    }
+    ValidarPersonaInglesa.prototype.isValid = function (persona) {
+        return persona.identificativo != "" && persona.primerNombre != "" && persona.nombreIntermedio != "" && persona.apellido1 != "";
+    };
+    return ValidarPersonaInglesa;
+}());
+var ValidarPersonaEspanyola = /** @class */ (function () {
+    function ValidarPersonaEspanyola() {
+    }
+    ValidarPersonaEspanyola.prototype.isValid = function (persona) {
+        return persona.identificativo != "" && persona.primerNombre != "" && persona.apellido1 != "" && persona.apellido2 != "";
+    };
+    return ValidarPersonaEspanyola;
+}());
+var MostrarPersonaInglesa = /** @class */ (function () {
+    function MostrarPersonaInglesa() {
+    }
+    MostrarPersonaInglesa.prototype.mostrarDatos = function (persona) {
+        var estaActivo = "No";
+        if (persona.activo) {
+            estaActivo = "Si";
+        }
+        return "<p class=\"my-1\"><strong>Campos obligatorios</strong></p>\n                <p class=\"mb-1\">Identificativo: ".concat(persona.identificativo, "</p>\n                <p class=\"mb-1\">Primer Nombre: ").concat(persona.primerNombre, "</p>\n                <p class=\"mb-1\">Nombre Intermedio: ").concat(persona.nombreIntermedio, "</p>\n                <p class=\"mb-3\">Apellido 1: ").concat(persona.apellido1, "</p>\n                <p class=\"mb-1\"><strong>Campos optativos</strong></p>\n                <p class=\"mb-1\">Apellido 2: ").concat(persona.apellido2, "</p>\n                <p class=\"mb-1\">A\u00F1o Nacimiento: ").concat(persona.anyoNacimiento, "</p>\n                <p class=\"mb-1\">Activo: ").concat(estaActivo, "</p>");
+    };
+    return MostrarPersonaInglesa;
+}());
+var MostrarPersonaEspanyola = /** @class */ (function () {
+    function MostrarPersonaEspanyola() {
+    }
+    MostrarPersonaEspanyola.prototype.mostrarDatos = function (persona) {
+        var estaActivo = "No";
+        if (persona.activo) {
+            estaActivo = "Si";
+        }
+        return "<p class=\"my-1\"><strong>Campos obligatorios</strong></p>\n                <p class=\"mb-1\">Identificativo: ".concat(persona.identificativo, "</p>\n                <p class=\"mb-1\">Primer Nombre: ").concat(persona.primerNombre, "</p>\n                <p class=\"mb-1\">Apellido 1: ").concat(persona.apellido1, "</p>\n                <p class=\"mb-3\">Apellido 2: ").concat(persona.apellido2, "</p>\n                <p class=\"mb-1\"><strong>Campos optativos</strong></p>\n                <p class=\"mb-1\">Nombre Intermedio: ").concat(persona.nombreIntermedio, "</p>\n                <p class=\"mb-1\">A\u00F1o Nacimiento: ").concat(persona.anyoNacimiento, "</p>\n                <p class=\"mb-1\">Activo: ").concat(estaActivo, "</p>");
+    };
+    return MostrarPersonaEspanyola;
+}());
+document.getElementById("idBotonMostrar").addEventListener("click", visualizar);
+function visualizar() {
+    var nacionalidad = document.querySelector('input[name="nacionalidad"]:checked').value;
+    //console.log(nacionalidad);
+    var nombre = document.getElementById("idPrimerNombre").value;
+    var nomInt = document.getElementById("idNombreIntermedio").value;
+    var apell1 = document.getElementById("idApellido1").value;
+    var apell2 = document.getElementById("idApellido2").value;
+    var anyoNac = Number(document.getElementById("idAnyoNacimiento").value);
+    var ident = document.getElementById("idIdentificativo").value;
+    var activ = document.getElementById("idActivo").checked;
+    var _creadorPersona = new CrearPersona;
+    var persona = _creadorPersona.crearPersona(nombre, nomInt, apell1, apell2, anyoNac, ident, activ);
+    var _validarPersona;
+    var _mostrarPersona;
+    switch (nacionalidad) {
+        case "ESP":
+            console.log(nacionalidad + ": " + persona);
+            _validarPersona = new ValidarPersonaEspanyola;
+            _mostrarPersona = new MostrarPersonaEspanyola;
+            break;
+        case "ING":
+            console.log(nacionalidad + ": " + persona);
+            _validarPersona = new ValidarPersonaInglesa;
+            _mostrarPersona = new MostrarPersonaInglesa;
+            break;
+    }
+    if (_validarPersona.isValid(persona)) {
+        document.getElementById("ventanaVerde").innerHTML = _mostrarPersona.mostrarDatos(persona);
+        document.getElementById("ventanaRoja").innerHTML = "";
+    }
+    else {
+        document.getElementById("ventanaVerde").innerHTML = "";
+        document.getElementById("ventanaRoja").innerHTML = '<p class="text-center mb-0">NO VALIDA</P>';
+    }
+}
+//# sourceMappingURL=app.js.map
---
+++ EjercicioPersona/wwwroot/js/app.ts
@@ -0,0 +1,126 @@
+﻿class Persona {
+    primerNombre: string = "";
+    nombreIntermedio: string = "";
+    apellido1: string = "";
+    apellido2: string = "";
+    anyoNacimiento: number = 0;
+    identificativo: string = "";
+    activo: boolean = false;
+    //constructor(primerNombre: string, nombreIntermedio: string, apellido1: string, apellido2: string, anyoNacimiento: number, identificativo: string, activo: boolean) {
+    //    this.primerNombre = primerNombre;
+    //    this.nombreIntermedio = nombreIntermedio;
+    //    this.apellido1 = apellido1;
+    //    this.apellido2 = apellido2;
+    //    this.anyoNacimiento = anyoNacimiento;
+    //    this.identificativo = identificativo;
+    //    this.activo = activo;
+    //}
+}
+interface ICreadorPersona {
+    crearPersona(primerNombre: string, nombreIntermedio: string, apellido1: string, apellido2: string, anyoNacimiento: number, identificativo: string, activo: boolean): Persona;
+}
+class CrearPersona implements ICreadorPersona {
+    crearPersona(primerNombre: string, nombreIntermedio: string, apellido1: string, apellido2: string, anyoNacimiento: number, identificativo: string, activo: boolean): Persona {
+        let nuevaPersona = new Persona;
+        nuevaPersona.primerNombre = primerNombre;
+        nuevaPersona.nombreIntermedio = nombreIntermedio;
+        nuevaPersona.apellido1 = apellido1;
+        nuevaPersona.apellido2 = apellido2;
+        nuevaPersona.anyoNacimiento = anyoNacimiento;
+        nuevaPersona.identificativo = identificativo;
+        nuevaPersona.activo = activo;
+        return nuevaPersona;
+    }
+
+}
+interface IValidadorPersona {
+    isValid(persona: Persona): boolean;
+}
+class ValidarPersonaInglesa implements IValidadorPersona {
+    isValid(persona: Persona): boolean {
+        return persona.identificativo != "" && persona.primerNombre != "" && persona.nombreIntermedio != "" && persona.apellido1 != "";
+    }
+}
+class ValidarPersonaEspanyola implements IValidadorPersona {
+    isValid(persona: Persona): boolean {
+        return persona.identificativo != "" && persona.primerNombre != "" && persona.apellido1 != "" && persona.apellido2 != "";
+    }
+}
+interface IMostradorPersona {
+    mostrarDatos(persona: Persona): string;
+}
+class MostrarPersonaInglesa implements IMostradorPersona {
+    mostrarDatos(persona: Persona): string {
+        let estaActivo = "No";
+        if (persona.activo) {
+            estaActivo = "Si";
+        }
+        return `<p class="my-1"><strong>Campos obligatorios</strong></p>
+                <p class="mb-1">Identificativo: ${persona.identificativo}</p>
+                <p class="mb-1">Primer Nombre: ${persona.primerNombre}</p>
+                <p class="mb-1">Nombre Intermedio: ${persona.nombreIntermedio}</p>
+                <p class="mb-3">Apellido 1: ${persona.apellido1}</p>
+                <p class="mb-1"><strong>Campos optativos</strong></p>
+                <p class="mb-1">Apellido 2: ${persona.apellido2}</p>
+                <p class="mb-1">Año Nacimiento: ${persona.anyoNacimiento}</p>
+                <p class="mb-1">Activo: ${estaActivo}</p>`;
+    }
+}
+class MostrarPersonaEspanyola implements IMostradorPersona {
+    mostrarDatos(persona: Persona): string {
+        let estaActivo = "No";
+        if (persona.activo) {
+            estaActivo = "Si";
+        }
+        return `<p class="my-1"><strong>Campos obligatorios</strong></p>
+                <p class="mb-1">Identificativo: ${persona.identificativo}</p>
+                <p class="mb-1">Primer Nombre: ${persona.primerNombre}</p>
+                <p class="mb-1">Apellido 1: ${persona.apellido1}</p>
+                <p class="mb-3">Apellido 2: ${persona.apellido2}</p>
+                <p class="mb-1"><strong>Campos optativos</strong></p>
+                <p class="mb-1">Nombre Intermedio: ${persona.nombreIntermedio}</p>
+                <p class="mb-1">Año Nacimiento: ${persona.anyoNacimiento}</p>
+                <p class="mb-1">Activo: ${estaActivo}</p>`;
+    }
+}
+
+(<HTMLElement>document.getElementById("idBotonMostrar")).addEventListener("click", visualizar);
+function visualizar() {
+    let nacionalidad = (<HTMLInputElement>document.querySelector('input[name="nacionalidad"]:checked')).value;
+    //console.log(nacionalidad);
+    let nombre = (<HTMLInputElement>document.getElementById("idPrimerNombre")).value;
+    let nomInt = (<HTMLInputElement>document.getElementById("idNombreIntermedio")).value;
+    let apell1 = (<HTMLInputElement>document.getElementById("idApellido1")).value;
+    let apell2 = (<HTMLInputElement>document.getElementById("idApellido2")).value;
+    let anyoNac = Number((<HTMLInputElement>document.getElementById("idAnyoNacimiento")).value);
+    let ident = (<HTMLInputElement>document.getElementById("idIdentificativo")).value;
+    let activ = (<HTMLInputElement>document.getElementById("idActivo")).checked;
+
+    let _creadorPersona: ICreadorPersona = new CrearPersona;
+    let persona = _creadorPersona.crearPersona(nombre, nomInt, apell1, apell2, anyoNac, ident, activ);
+
+    let _validarPersona: IValidadorPersona;
+    let _mostrarPersona: IMostradorPersona;
+
+    switch (nacionalidad) {
+        case "ESP":
+            console.log(nacionalidad + ": " + persona);
+            _validarPersona = new ValidarPersonaEspanyola;
+            _mostrarPersona = new MostrarPersonaEspanyola;
+            break;
+        case "ING":
+            console.log(nacionalidad + ": " + persona);
+            _validarPersona = new ValidarPersonaInglesa;
+            _mostrarPersona = new MostrarPersonaInglesa;
+            break;
+    }
+
+    if (_validarPersona.isValid(persona)) {
+        (<HTMLElement>document.getElementById("ventanaVerde")).innerHTML = _mostrarPersona.mostrarDatos(persona);
+        (<HTMLElement>document.getElementById("ventanaRoja")).innerHTML = "";
+    }
+    else {
+        (<HTMLElement>document.getElementById("ventanaVerde")).innerHTML = "";
+        (<HTMLElement>document.getElementById("ventanaRoja")).innerHTML = '<p class="text-center mb-0">NO VALIDA</P>';
+    }
+}
