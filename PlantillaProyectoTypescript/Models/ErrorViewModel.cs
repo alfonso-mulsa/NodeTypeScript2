d9d042d835fd65e918065c5f95d57466ca22fba7
Alfonso Mulsa Fau <amulsa@gmail.com>
2024-04-15 14:16:04 +0200

    Borrar plantillas

--- PlantillaProyectoTypescript/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PlantillaProyectoTypescript.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
